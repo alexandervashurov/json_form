1a3d2331b3219a79a0b3f38f4c8e2ac7a526190e
alexandervashurov <mazda5000@mail.ru>
2018-03-14 19:32:25 +0300

    fix Server javadoc

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,17 @@
 
+
+/**
+ * Interface of simple server
+ */
+
 public interface Server {
-   
+    /**
+     * Starts server
+     */
     void start();
 
-   
+    /**
+     * Stops currently running server
+     */
     void stop();
 }
